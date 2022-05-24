10db4efb83a93df4cc71ed1dc990070a4b2c5b90
Serhiy Kolesnyk <serhiy.kolesnyk@ag-space.com>
2022-05-24 16:27:14 +0300

    main- video-block last update

--- src/app/components/video-block/video-block.component.ts
+++ src/app/components/video-block/video-block.component.ts
@@ -8,11 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class VideoBlockComponent implements OnInit {
 
   mainTitle = 'Our Work Process';
-  description = "Was years it seasons was there form he in in them together over that, third sixth gathered female creeping bearing behold years."
+  description = 'Was years it seasons was there form he in in them together over that, third sixth gathered female creeping bearing behold years.';
+  // Нюанс. Використовувати тільки одинарні скобки в ts-js файлах. + завжди ставити я то вже сам зміню. ; 
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
