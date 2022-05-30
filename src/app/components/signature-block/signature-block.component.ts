d29caa29fbd24daee3623daadf016b37d839ddd9
Maks Delta <mc.pullupenko@gmail.com>
2022-05-30 23:04:31 +0300

    made HTML document

--- src/app/components/signature-block/signature-block.component.ts
+++ src/app/components/signature-block/signature-block.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignatureBlockComponent implements OnInit {
 
+  mainTitle = 'About Us'
+  description = `Divide have don't man wherein air fourth. Own itself make have night won't make. A you under Seed appear which good give. Own give air without fowl moveth dry first heaven fruit, dominion she'd won't very all.`
+
   constructor() { }
 
   ngOnInit(): void {
