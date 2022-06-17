82c52963e9f69d4d9a08147ac179d6d7e6599298
Maks Delta <mc.pullupenko@gmail.com>
2022-06-17 19:30:33 +0300

    add ts file

--- src/app/components/skills-block/skills-block.component.ts
+++ src/app/components/skills-block/skills-block.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsBlockComponent implements OnInit {
 
+  titleLoad = 'Professional Skills';
+  descDesign = 'UI/UX Design';
+  progLoadDesign = '75%';
+  descDevelopment = 'web Development';
+  progLoadDevelopment = '90%';
+  descMarketing = 'Marketing';
+  progLoadMarketing = '65%';
+
   constructor() { }
 
   ngOnInit(): void {
